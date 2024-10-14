82c813ea1b42924df73489724b50a39968ab807f
Tran Van Luong <tranvanluong032020@gmail.com>
2024-10-15 00:05:01 +0700

    add staffSign and satffSignUp, update staff Model

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -4,6 +4,7 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const ApiError = require('../api-error')
 const userService = require('../services/user.service')
+const staffService = require('../services/staff.service')
 
 module.exports.signUp =  async (req, res, next ) =>{
     try {
@@ -33,10 +34,30 @@ module.exports.signIn =  async (req, res, next) =>{
     }
 }
 
-module.exports.staffSignIn = () =>{
-
+module.exports.staffSignIn = async (req, res , next) =>{
+    try {
+        const data = req.body
+        const staff = new staffService()
+        const signInResult = await staff.signIn(data)
+        res.status(200).json({
+            status: 200,
+            message: signInResult.message,
+        });
+    } catch (error) {
+        return next(new ApiError("500", "An error occurred while Signing in !!!"));
+    }
 }
 
-module.exports.staffSignUp = () =>{
-
+module.exports.staffSignUp = async (req, res, next) =>{
+    try {
+        const data = req.body
+        const staff = new staffService()
+        const signUpResult = await staff.signUp(data)
+        res.status(200).json({
+            status:200,
+            message: signUpResult.message
+        })
+    } catch (error) {
+        return next(new ApiError("500", "An error occurred while Signing up !!!"));
+    }
 }
